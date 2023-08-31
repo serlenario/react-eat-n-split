05f59c04de457422e20c7ebf2f024ab62c26aeb2
Lenar <renepelicion@gmail.com>
2023-08-31 20:40:17 +0300

    edit: vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/react-eat-n-list',
+	base: '/react-eat-n-split',
 	plugins: [react()],
 });
